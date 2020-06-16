4b61b8ac69f3066822b6b7d98480f6a98acbf3b8
Ian Scott <chesnekov@gmail.com>
2020-06-16 13:35:50 -0700

    add business logic to create movie and ticket object types, as well as an age checking method and an assign Ticket Price method
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+function Ticket (movie, movieTime, age) {
+  this.movie = movie;
+  this.movieTime = movieTime;
+  this.age = age;  
+}
+
+Ticket.prototype.assignTicketPrice = function () {
+  
+  if (this.movie.isNewRelease) {
+    this.price = 15;
+  } else {
+    this.price = 12;
+  }
+
+  if (this.movieTime === "evening") {
+    this.price += 3;
+  } else if (this.movieTime === "morning") {
+    this.price -= 2;
+  }
+
+  if (this.age >= 64 || this.age <= 12) {
+    this.price -= 3;
+  }
+}
+
+Ticket.prototype.allowsKids = function () {
+  if (this.age <= this.movie.minAge) {
+    alert(`You must be ${this.movie.minAge} years old to watch ${this.movie.title}`);
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function Movie (title, isNewRelease, minAge) {
+  this.title = title;
+  this.isNewRelease = isNewRelease;
+  this.minAge = minAge;
+}
+
+let movie1 = new Movie("Forest Gump", false, 13);
+let movie2 = new Movie("Frozen", false, 0);
+let movie3 = new Movie("Train to Busan", true, 18);
+let movie4 = new Movie ("Tenant", true, 18);
+let movie5 = new Movie ("Trolls: World Tour", true, 0);
+let movie6 = new Movie("Saw", false, 18);
